7762bb1834607b4367a68f58df6839f22694d50b
purusoth-ideas2it <purushothaman.kabaleeswaran@ideas2it.com>
2023-01-31 20:09:44 +0530

    Added functions to mark the completed task and delete task

--- todo.js
+++ todo.js
@@ -3,7 +3,6 @@ let displayList = document.getElementById("taskList");
 
 function addTask(newTask) {
     toDoList.push(newTask)
-    alert("Task Added Successfuly");
     document.getElementById("taskName").value = "";
     displayTask(newTask);
 }
@@ -12,12 +11,51 @@ function displayTask(newTask) {
     let task = document.createElement("li");
     let checkbox = document.createElement("input");
     let displayText = document.createElement("label");
+    let customCheckBox = document.createElement("span");
+    let deleteIcon = document.createElement("i");
+    deleteIcon.setAttribute("class", "fa-solid fa-trash")
+    deleteIcon.setAttribute("onclick",
+        "deleteTask(this)");
+    customCheckBox.setAttribute("class", "checkmark");
     displayText.innerText = newTask;
+    displayText.setAttribute("class", "display-text");
     checkbox.type = "checkbox";
-    checkbox.setAttribute("class", "check-box");
-    checkbox.setAttribute("value", newTask);
-    task.appendChild(checkbox);
+    checkbox.className = "check-box";
+    checkbox.value = newTask;
+    checkbox.setAttribute("onchange", "completedTask(value, checked, this)");
+    displayText.appendChild(checkbox);
+    displayText.appendChild(customCheckBox);
     task.appendChild(displayText);
+    task.appendChild(deleteIcon);
     task.setAttribute("class", 'task-list');
     displayList.appendChild(task);
+}
+
+function displayCompletedTask() {
+
+    if (null == document.getElementById("cosfdfdsafd")) {
+        let div = document.createElement("div");
+        let completedHeader = document.createElement("div");
+        let completedButton = document.createElement("Button");
+        div.className = "completed-container";
+        div.id = "cosfdfdsafd"
+        completedHeader.innerText = "Completed Task";
+        div.appendChild(completedButton);
+        div.appendChild(completedHeader);
+        document.body.appendChild(div);
+    }
+}
+
+function completedTask(value, checked, element) {
+    if (checked) {
+        console.log(element.prevoiusSibling);
+        displayCompletedTask();
+        deleteTask(element.parentNode);
+    } else {
+        displayTask(value);
+    }
+}
+
+function deleteTask(value) {
+    value.parentElement.remove();
 }
