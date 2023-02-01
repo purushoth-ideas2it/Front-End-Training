a8196d16826861618fe8e489e815ecc4f74bd6f2
purusoth-ideas2it <purushothaman.kabaleeswaran@ideas2it.com>
2023-02-01 18:58:56 +0530

    added funtion to display and hide completed task and confirm before deleting

--- todo.js
+++ todo.js
@@ -31,31 +31,110 @@ function displayTask(newTask) {
     displayList.appendChild(task);
 }
 
-function displayCompletedTask() {
+function displayCompletedTask(taskValue) {
 
-    if (null == document.getElementById("cosfdfdsafd")) {
-        let div = document.createElement("div");
+    if (null == document.getElementById("completedContainer")) {
+        let completedContainer = document.createElement("div");
         let completedHeader = document.createElement("div");
-        let completedButton = document.createElement("Button");
-        div.className = "completed-container";
-        div.id = "cosfdfdsafd"
-        completedHeader.innerText = "Completed Task";
-        div.appendChild(completedButton);
-        div.appendChild(completedHeader);
-        document.body.appendChild(div);
+        let completedBody = document.createElement("div");
+        let completedIcon = document.createElement("i");
+        let completedHeaderText = document.createElement("div");
+        let completedTaskList = document.createElement("ul");
+        completedContainer.id = "completedContainer";
+        completedHeader.className = "completed-header";
+        completedHeader.setAttribute("onclick", "collapsible()");
+        completedBody.className = "completed-body";
+        completedBody.id = "completedBody";
+        completedIcon.className = "fas fa-angle-right";
+        completedIcon.id = "collapseButton";
+        completedHeaderText.className = "completed-header-text";
+        completedHeaderText.id = "completed-header-text";
+        completedHeaderText.innerText = "Completed";
+        completedTaskList.id = "completedTaskList";
+        completedHeader.appendChild(completedIcon);
+        completedHeader.appendChild(completedHeaderText);
+        completedBody.appendChild(completedTaskList);
+        completedContainer.appendChild(completedHeader);
+        completedContainer.appendChild(completedBody);
+        document.body.appendChild(completedContainer);
     }
+    addToCompletedList(taskValue);
+}
+
+function addToCompletedList(taskValue) {
+    let displayCompletedList = document.getElementById("completedTaskList");
+    let task = document.createElement("li");
+    let checkbox = document.createElement("input");
+    let displayText = document.createElement("label");
+    let completedTask = document.createElement("del");
+    let customCheckBox = document.createElement("span");
+    let deleteIcon = document.createElement("i");
+    deleteIcon.setAttribute("class", "fa-solid fa-trash")
+    deleteIcon.setAttribute("onclick",
+        "deleteTask(this)");
+    customCheckBox.setAttribute("class", "checkmark");
+    completedTask.innerText = taskValue;
+    displayText.setAttribute("class", "display-text");
+    checkbox.type = "checkbox";
+    checkbox.checked = "true";
+    checkbox.className = "check-box";
+    checkbox.value = taskValue;
+    checkbox.setAttribute("onchange", "completedTask(value, checked, this)");
+    displayText.appendChild(completedTask);
+    displayText.appendChild(checkbox);
+    displayText.appendChild(customCheckBox);
+    task.appendChild(displayText);
+    task.appendChild(deleteIcon);
+    task.setAttribute("class", 'task-list');
+    displayCompletedList.appendChild(task);
 }
 
 function completedTask(value, checked, element) {
+
     if (checked) {
-        console.log(element.prevoiusSibling);
-        displayCompletedTask();
-        deleteTask(element.parentNode);
+        displayCompletedTask(value);
+        removeTask(element.parentNode);
+        updateCompleteCount();
     } else {
         displayTask(value);
+        removeTask(element.parentNode);
+        removeCompleteBlock();
+        updateCompleteCount();
     }
 }
 
 function deleteTask(value) {
+    if (confirm("Confirm delete")) {
+        value.parentElement.remove();
+        removeCompleteBlock();
+        updateCompleteCount();
+    }
+}
+
+function removeTask(value) {
     value.parentElement.remove();
+    removeCompleteBlock();
+    updateCompleteCount();
+}
+
+function removeCompleteBlock() {
+    if (null != document.getElementById("completedContainer") && 0 === document.getElementById("completedTaskList").childElementCount) {
+        document.getElementById("completedContainer").remove();
+    }
+}
+
+function collapsible() {
+    if ("fas fa-angle-down" == document.getElementById("collapseButton").className) {
+        document.getElementById("collapseButton").className = "fas fa-angle-right";
+        document.getElementById("completedBody").style.display = "none";
+    } else {
+        document.getElementById("collapseButton").className = "fas fa-angle-down";
+        document.getElementById("completedBody").style.display = "block";
+    }
+}
+
+function updateCompleteCount() {
+    if (null != document.getElementById("completedContainer")) {
+        document.getElementById("completed-header-text").innerText = "Completed  " + document.getElementById("completedTaskList").childElementCount;
+    }
 }
