150897e17659f276d1d2b0edca69a27e88623940
purusoth-ideas2it <purushothaman.kabaleeswaran@ideas2it.com>
2023-02-02 19:54:34 +0530

    added funtion to update the task

--- todo.js
+++ todo.js
@@ -2,33 +2,93 @@ let toDoList = [];
 let displayList = document.getElementById("taskList");
 
 function addTask(newTask) {
-    toDoList.push(newTask)
-    document.getElementById("taskName").value = "";
-    displayTask(newTask);
+    if ("" != newTask) {
+        toDoList.push(newTask)
+        document.getElementById("taskName").value = "";
+        displayTask(newTask);
+    } else {
+        alert("Cannot Add Empty Task");
+    }
 }
 
 function displayTask(newTask) {
     let task = document.createElement("li");
     let checkbox = document.createElement("input");
-    let displayText = document.createElement("label");
-    let customCheckBox = document.createElement("span");
-    let deleteIcon = document.createElement("i");
-    deleteIcon.setAttribute("class", "fa-solid fa-trash")
-    deleteIcon.setAttribute("onclick",
-        "deleteTask(this)");
-    customCheckBox.setAttribute("class", "checkmark");
+    let displayTextContainer = document.createElement("label");
+    let displayText = document.createElement("span");
     displayText.innerText = newTask;
+    let customCheckBox = document.createElement("span");
+    let editIcon = document.createElement("i");
+    let deleteIcon = document.createElement("i");
+    editIcon.className = "far fa-edit";
+    editIcon.setAttribute("onclick", "editTask(this)");
+    deleteIcon.setAttribute("class", "fa-solid fa-trash")
+    // deleteIcon.setAttribute("onclick",
+    // "deleteTask(this)");
+    customCheckBox.setAttribute("class", "checkmark");
+    // displayText.innerText = newTask;
     displayText.setAttribute("class", "display-text");
     checkbox.type = "checkbox";
     checkbox.className = "check-box";
     checkbox.value = newTask;
     checkbox.setAttribute("onchange", "completedTask(value, checked, this)");
-    displayText.appendChild(checkbox);
-    displayText.appendChild(customCheckBox);
+    displayTextContainer.appendChild(checkbox);
+    displayTextContainer.appendChild(customCheckBox);
+    task.appendChild(displayTextContainer);
     task.appendChild(displayText);
+    task.appendChild(editIcon);
     task.appendChild(deleteIcon);
     task.setAttribute("class", 'task-list');
+    displayList.addEventListener('click', display);
     displayList.appendChild(task);
+}
+
+function editTask(element) {
+    saveButton = document.createElement("button");
+    cancelButton = document.createElement("i");
+    cancelButton.className = "fa fa-times";
+    cancelButton.setAttribute("onclick", "cancel(this.previousSibling)");
+    saveButton.id = "save-button";
+    saveButton.innerText = "SAVE";
+    saveButton.setAttribute("onclick", "updateTask(this)");
+    element.style.display = "none";
+    element.nextSibling.style.display = "none";
+    element.previousSibling.contentEditable = true;
+    element.previousSibling.style.outline = "none";
+    element.previousSibling.focus();
+    element.parentElement.appendChild(saveButton);
+    element.parentElement.appendChild(cancelButton);
+}
+
+function updateTask(element) {
+    let task = element.previousSibling.previousSibling.previousSibling.innerText;
+    console.log(task);
+    if (0 == task.length) {
+        alert("cannot Update Task");
+        element.previousSibling.previousSibling.previousSibling.focus();
+        return;
+    }
+    element.previousSibling.style.display = "block";
+    element.previousSibling.previousSibling.style.display = "block";
+    element.previousSibling.previousSibling.previousSibling.contentEditable = false;
+    element.nextSibling.remove();
+    element.remove();
+}
+
+function cancel(element) {
+    let editTaskElement = element.previousSibling.previousSibling.previousSibling;
+    editTaskElement.contentEditable = false;
+    editTaskElement.innerText = editTaskElement.previousSibling.firstChild.value;
+    element.previousSibling.style.display = "block";
+    element.previousSibling.previousSibling.style.display = "block";
+    element.nextSibling.remove();
+    element.remove();
+}
+
+function display(event) {
+    if (event.target.className == "fa-solid fa-trash") {
+        deleteTask(event.target.parentNode)
+    }
 }
 
 function displayCompletedTask(taskValue) {
@@ -40,6 +100,7 @@ function displayCompletedTask(taskValue) {
         let completedIcon = document.createElement("i");
         let completedHeaderText = document.createElement("div");
         let completedTaskList = document.createElement("ul");
+        completedTaskList.addEventListener('click', display);
         completedContainer.id = "completedContainer";
         completedHeader.className = "completed-header";
         completedHeader.setAttribute("onclick", "collapsible()");
@@ -70,8 +131,8 @@ function addToCompletedList(taskValue) {
     let customCheckBox = document.createElement("span");
     let deleteIcon = document.createElement("i");
     deleteIcon.setAttribute("class", "fa-solid fa-trash")
-    deleteIcon.setAttribute("onclick",
-        "deleteTask(this)");
+    // deleteIcon.setAttribute("onclick",
+    //     "deleteTask(this)");
     customCheckBox.setAttribute("class", "checkmark");
     completedTask.innerText = taskValue;
     displayText.setAttribute("class", "display-text");
@@ -105,7 +166,7 @@ function completedTask(value, checked, element) {
 
 function deleteTask(value) {
     if (confirm("Confirm delete")) {
-        value.parentElement.remove();
+        value.remove();
         removeCompleteBlock();
         updateCompleteCount();
     }
